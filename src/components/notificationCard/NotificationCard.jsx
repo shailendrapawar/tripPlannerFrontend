ad76214417be1d47f55e58fc3d9f86bbbb56b44f
shailendr pawar <shailendrapawar980@gmail.com>
2025-03-12 12:02:57 +0530

    ADDED: fucntionality for rejecting a user request

--- src/components/notificationCard/NotificationCard.jsx
+++ src/components/notificationCard/NotificationCard.jsx
@@ -8,38 +8,78 @@ import { useState } from "react";
 import { removeUserNotification } from "../../store/slice/userSlice";
 import { toast } from "react-hot-toast"
 const NotificationCard = ({ data }) => {
-  // console.log(data)
+  console.log(data)
   const { theme } = useSelector(state => state.theme)
   const { authUser } = useSelector(s => s.user)
   const dispatch = useDispatch()
 
   const [loading, setLoading] = useState(false)
 
-  const approveUser = async () => {
-    const isApprove = await axios.post(import.meta.env.VITE_API_URL + `/trip/approveUser/${data.senderId}`, {
-      tripId: data.relatedTo,
-      userName: authUser.name
-    }, {
-      withCredentials: true
-    })
-    if (isApprove) {
 
-      const isNotifyDelete = await axios.post(import.meta.env.VITE_API_URL + `/trip/deleteNotification/${data._id}`, {}, {
+
+  const approveUser = async () => {
+    try {
+      setLoading(true)
+      const isApprove = await axios.post(import.meta.env.VITE_API_URL + `/trip/approveUser/${data.senderId}`, {
+        tripId: data.relatedTo,
+        userName: authUser.name
+      }, {
         withCredentials: true
       })
+      if (isApprove) {
 
-      if (isNotifyDelete) {
-        dispatch(removeUserNotification(data))
-        toast.success(`user request approved`)
+        const isNotifyDelete = await axios.post(import.meta.env.VITE_API_URL + `/trip/deleteNotification/${data._id}`, {}, {
+          withCredentials: true
+        })
+
+        if (isNotifyDelete) {
+          dispatch(removeUserNotification(data))
+          toast.success(`user request approved`)
+        }
       }
+
+    } catch (err) {
+      console.log(err)
+    }finally{
+      setLoading(false)
     }
   }
 
 
-
   const rejectUser = async () => {
-    console.log("reject")
+
+    try {
+      setLoading(true)
+      const isRejected = await axios.post(import.meta.env.VITE_API_URL + `/trip/rejectUser/${data.senderId}`, {
+        tripId: data.relatedTo,
+        userName: authUser.name
+      }, {
+        withCredentials: true
+      })
+
+      if (isRejected) {
+
+        const isNotifyDelete = await axios.post(import.meta.env.VITE_API_URL + `/trip/deleteNotification/${data._id}`, {}, {
+          withCredentials: true
+        })
+
+        if (isNotifyDelete) {
+          dispatch(removeUserNotification(data))
+          toast.success(`user request approved`)
+        }
+      }
+
+    } catch (err) {
+      console.log(err)
+    }finally{
+      setLoading(false)
+    }
   }
+
+  const formatDate = (isoDate) => {
+    return new Date(isoDate).toLocaleDateString("en-GB", { day: "numeric", month: "numeric"});
+  };
+  const getTime = (timestamp) => new Date(timestamp).toLocaleTimeString('en-GB', { hour: '2-digit', minute: '2-digit' });
 
   const isApproveRequest = data.type === "join_request"
   // console.log(isApproveRequest)
@@ -49,16 +89,16 @@ const NotificationCard = ({ data }) => {
         <MiniLoader value={loading} />
       </span>
       <img src={sampleUserAvatar} className="w-12 h-12 rounded-full bg-gray-200 p-1"></img>
-      
+
       <div className="h-15 w-[65%] relative">
         <section className="text-xs text-center mt-2" style={{ color: theme.dark }}>{data.message}</section>
 
         <div className="relative h-8" style={isApproveRequest ? { display: "block" } : { display: "none" }}>
-          <button className="absolute bottom-0 right-20 w-18 text-xs h-6 rounded-sm bg-red-500 text-white" onClick={rejectUser}>REJECT</button>
-          <button className="absolute bottom-0 right-0 w-18 text-xs h-6 rounded-sm bg-blue-500 text-white" onClick={approveUser}>APPROVE</button>
+          <button className="absolute bottom-0 left-20 w-18 text-xs h-6 rounded-sm bg-red-500 text-white" onClick={rejectUser}>REJECT</button>
+          <button className="absolute bottom-0 left-0 w-18 text-xs h-6 rounded-sm bg-blue-500 text-white" onClick={approveUser}>APPROVE</button>
         </div>
       </div>
-      <i className="text-xs absolute right-2 bottom-1" style={{ color: theme.dark }}>23:50</i>
+      <i className="text-xs absolute right-2 bottom-1" style={{ color: theme.dark }}>{getTime(data?.createdAt)}</i>
     </div>
   )
 }
